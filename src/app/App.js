d3ecd9ecb7e632a372b8f93d3e2f0f5474af4198
tolerevo <tolerevo@hotmail.com>
2024-01-22 06:24:53 -0600

    step 5

--- src/app/App.js
+++ src/app/App.js
@@ -10,15 +10,26 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import "./App.css";
 
-const router = (
-  /* Wrap this Root Route to create Router here */
-  <Route path="/" element={<Root />}>
-    {/* Add Routes here! */}
-  </Route>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    /* Wrap this Root Route to create Router here */
+    <Route path="/" element={<Root />}>
+      <Route path="about" element={<About />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="articles" element={<Articles />} />
+      <Route path="categories" element={<Categories />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  ),
 );
 
 function App() {

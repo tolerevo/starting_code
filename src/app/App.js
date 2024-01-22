f706aa5815f8afb5cee4b6ed4e09a84bd029d871
tolerevo <tolerevo@hotmail.com>
2024-01-22 06:56:32 -0600

    nestes routes

--- src/app/App.js
+++ src/app/App.js
@@ -26,12 +26,16 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="sign-up" element={<SignUp />} />
       <Route path="articles" element={<Articles />} />
-      <Route path="articles/:title" element={ <Article/> } />
-      <Route path="authors/:name" element={ <Author/> } />
-      <Route path="categories" element={<Categories />} />
-      <Route path="profile" element={<Profile />} />
-    </Route>
-  )
+      <Route path="articles/:title" element={<Article />} />
+      <Route path="authors/:name" element={<Author />} />
+      <Route path="categories" element={<Categories />}>
+        <Route path=":name" element={<Category />} />
+      </Route>
+      <Route path="profile" element={<Profile />}>
+        <Route path="edit" element={<EditProfileForm />} />
+      </Route>
+    </Route>,
+  ),
 );
 
 function App() {

e746bfab6becea07eea215dfc69b1fd1714b99e6
tolerevo <tolerevo@hotmail.com>
2024-01-22 07:04:16 -0600

    navigation

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet} from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile () {
@@ -8,7 +8,9 @@ export default function Profile () {
   const loggedIn = useSelector(selectIsLoggedIn);
   
   // use loggedIn to return a Navigate
-
+  if (!loggedIn) {
+    return <Navigate to="/sign-up"/>
+  }
   return (
     <main>
       <h1>{currentUser.username}</h1>
